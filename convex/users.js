6e4674f224dc1a0245f003eee6f71f7bb0c98808
Aprajita <beliefinaprajita@gmail.com>
2026-03-03 01:49:12 +0530

    Auth and User Info schema done

---
+++ convex/users.js
@@ -0,0 +1,26 @@
+import { mutation } from "./_generated/server";
+
+export const CreateUser=mutation({
+    args:{
+        name:v.string(),
+        emeail:v.string()
+    },
+    handler:async(convexToJson,args)=>{
+        //if user already exists
+        const userData=await ctx.db.query('users').filter(q=>q.eq(q.field('email'),args.email)).collect()
+        //if not then add new user
+        if(userData?.length==0){
+            const data={
+                name:args.name,
+                email:args.email,
+                credits:50000
+            }
+            const result=await ctx.db.insert('users',{
+                ...data
+            });
+            console.log(result);
+            return data;
+        }
+        return userData[0];
+    }
+})
